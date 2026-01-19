1e407cedc887c28a242fbaacc7e8aaf4f9cccf28
Gordon Ruleman <gpcreativestudios2018@gmail.com>
2026-01-18 19:05:19 -0500

    Separate vitest config to fix TypeScript build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -78,10 +77,5 @@ export default defineConfig({
         },
       },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
   },
 })
